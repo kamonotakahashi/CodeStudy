38c9f96779d31b9f96e074277a1b2cf7aff3754c
kamonotakahashi <kamonotakahashi@gmail.com>
2020-01-14 23:51:56 +0900

    Update abc002_4.py

--- python/abc002_4.py
+++ python/abc002_4.py
@@ -1,7 +1,9 @@
 import sys
-in = input().split(' ')
-N = in[0]
-M = in[1]
-if not (1 <= int(N) <= 12) or not (0 <= int(M) <= floor((N*N-N)/2)):
+import math
+io = input().split(' ')
+N = int(io[0])
+M = int(io[1])
+if not (1 <= N <= 12) or not (0 <= M <= math.floor((N*N-N)/2)):
     sys.exit()
-print("a")
+for l in range(0, M):
+    print(l)
