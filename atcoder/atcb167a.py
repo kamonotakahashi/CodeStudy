6f1879d51ca8cd220b5095076843fdf311d8c9bf
kamonotakahashi <kamonotakahashi@gmail.com>
2020-05-12 20:06:54 +0900

    Update atcb167a.py

--- atcoder/atcb167a.py
+++ atcoder/atcb167a.py
@@ -3,10 +3,14 @@ import re
 inp = []
 for cnt in range(2):
   inp.append(input())
-  if not 1 <= len(inp[cnt]) <= 10:
-    sys.exit()
   if not re.match(r"^[a-z]+$", inp[cnt]):
     sys.exit()
+
+cnt = 0
+for l in inp:
+  if not 1 <= len(inp[cnt]) <= (10 + cnt):
+    sys.exit()
+  cnt = cnt + 1
 
 if len(inp[0]) < len(inp[1]):
   if(inp[0] == inp[1][:-1]):
