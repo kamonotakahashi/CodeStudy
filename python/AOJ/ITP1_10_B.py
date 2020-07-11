466ddf879996a10fbbd53cee7c9200b237aa7870
sokorahen-szk <kamonotakahashi@gmail.com>
2020-07-12 00:24:52 +0900

    commit

--- python/AOJ/ITP1_10_B.py
+++ python/AOJ/ITP1_10_B.py
@@ -1,4 +1,11 @@
 import math
 a, b, C = list(map(int, input().split(' ')))
 c = math.sqrt(a**2 + b**2)
-pi = math.acos(-1)
+PI = math.acos(-1)
+rad = C * PI / 180
+h = b * math.sin(rad)
+S = 1/2*a*h
+L = a + b + math.sqrt(a**2 + b**2 - 2*a*b * math.cos(rad))
+print("{0:.8f}".format(S))
+print("{0:.8f}".format(L))
+print("{0:.8f}".format(h))
